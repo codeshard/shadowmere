a6a08e770177c89b203a176b6ba3eed25f27af38
Ozkar L. Garcell <ozkar.garcell@gmail.com>
2023-12-10 22:17:09 +0100

    feat: executing correct settings

--- shadowmere/celery.py
+++ shadowmere/celery.py
@@ -2,7 +2,10 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shadowmere.settings")
+if os.getenv("DEBUG") == "True":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shadowmere.settings.dev")
+else:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shadowmere.settings.prod")
 
 app = Celery("shadowmere")
 app.config_from_object("django.conf:settings", namespace="CELERY")
